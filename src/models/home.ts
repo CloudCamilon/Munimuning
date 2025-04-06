3abb30a9545e1da1f21a4a262bfa818cff2c5d6f
Vincent Ulap Camilon <vincentulapcamilon@P11-CLOUD-MAC.local>
2025-04-07 00:55:20 +0800

    chore: resolving netlify build issue - deleting and inserting file

---
+++ src/models/home.ts
@@ -0,0 +1,14 @@
+export type TProducts = {
+    title: string;
+    price?: string;
+    category?: string;
+    src: string;
+    alt: string;
+  };
+  
+  export type THomeSection = {
+    layoutClassName?: string;
+    title: string;
+    products: TProducts[];
+  };
+  
